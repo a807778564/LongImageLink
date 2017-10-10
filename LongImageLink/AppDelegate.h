cd0b82366a64f3bd5dfc701215fcf0e3f6075c1c
huangrensheng <807778564@qq.com>
2017-10-10 13:12:37 +0800

    init project

---
+++ LongImageLink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LongImageLink
+//
+//  Created by huangrensheng on 2017/10/10.
+//  Copyright © 2017年 laber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LongImageLink/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LongImageLink
+//
+//  Created by huangrensheng on 2017/10/10.
+//  Copyright © 2017年 laber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
